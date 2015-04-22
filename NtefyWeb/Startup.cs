bd45f09fbb6bbc40d66014a24e28e63590eae8a4
Andreas <carlsson75@gmail.com>
2015-04-22 20:55:22 +0200

    Added new project

---
+++ NtefyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NtefyWeb.Startup))]
+namespace NtefyWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
